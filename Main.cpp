c09bc7257a96e5d24feb0d632f4b29fa2bff4248
Max Koltovskiy <mkoltovskiy@gmail.com>
2023-03-18 19:41:07 -0400

    Twisty Maze

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,7 @@ const std::string kYourName = "Max";
 
 /* Change these constants to contain the paths out of your mazes. */
 const std::string kPathOutOfNormalMaze = "ESWSESWEENESNNNW";
-const std::string kPathOutOfTwistyMaze = "TODO: Replace this string with your path out of the twisty maze.";
+const std::string kPathOutOfTwistyMaze = "WESSNWSSNN";
 
 int main() {
     /* Generate the maze.
