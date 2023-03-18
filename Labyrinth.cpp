9edba70b39c899d4ffa104161d5d7223c4c52aa3
Max Koltovskiy <mkoltovskiy@gmail.com>
2023-03-18 18:33:21 -0400

    Normal Maze

---
+++ Labyrinth.cpp
@@ -0,0 +1,37 @@
+#include "Labyrinth.h"
+#include <iostream>
+bool isPathToFreedom(MazeCell* start, const std::string& moves, int found) {
+    if(start == nullptr) {
+        return false; 
+    }
+
+    if(start -> whatsHere != Item::NOTHING) {
+        found++;
+    }
+
+    //std::cout<< found << moves<<"\n";
+    if(moves == "" && found == 3) {
+        return true;
+    } else if(moves == "") {
+        return false;
+    }
+    
+    
+    std::cout<< moves[0]<< "\n";
+    switch(moves[0]) {
+        case 'N':
+            return isPathToFreedom(start -> north, moves.substr(1), found);
+            break;
+        case 'E':
+            return isPathToFreedom(start -> east, moves.substr(1), found);
+            break;
+        case 'S':
+            return isPathToFreedom(start -> south, moves.substr(1), found);
+            break;
+        case 'W':
+            return isPathToFreedom(start -> west, moves.substr(1), found);
+            break;
+    }
+
+    return false;
+}
